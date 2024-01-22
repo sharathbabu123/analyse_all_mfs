0f2b233ed555ac70e261d2c767759c13401d0a6a
sharathbabu123 <sharathr013@gmail.com>
2024-01-22 20:35:45 +0530

    added sector info

--- combine_all_mfs.py
+++ combine_all_mfs.py
@@ -2,24 +2,33 @@ import csv
 import datetime
 import os
 
+curent_directory = os.getcwd()
+
 # Get the current directory
-current_directory = r"C:\Users\shara\Desktop\gpt-bot\mutual_funds"
-print(current_directory)
+current_directory_portfolio = r"C:\Users\shara\Desktop\gpt-bot\mutual_funds\TOP_HOLDING"
+print(current_directory_portfolio)
+
+current_directory_sector_holding = r"C:\Users\shara\Desktop\gpt-bot\mutual_funds\TOP_SECTOR_HOLDING"
+print(current_directory_sector_holding)
 
 current_datetime = str(datetime.date.today())
 print(current_datetime)
 
 
-combined_file_path = current_directory+'\\'+'daily_mf_data\\'+'combined_'+current_datetime+'.csv'
+combined_file_path_portfolio = curent_directory+'\\daily_mf_data\\TOP_HOLDING'+'\\combined_portfolio_'+current_datetime+'.csv'
+combined_file_path_sector_holding = curent_directory+'\\daily_mf_data\\TOP_SECTOR_HOLDING'+'\\combined_sector_'+current_datetime+'.csv'
 
-# Check if the combined file already exists
-if os.path.exists(combined_file_path):
+if os.path.exists(combined_file_path_portfolio):
     # Empty the file
-    open(combined_file_path, 'w').close()
+    open(combined_file_path_portfolio, 'w').close()
+
+if os.path.exists(combined_file_path_sector_holding):
+    # Empty the file
+    open(combined_file_path_sector_holding, 'w').close()
 
 # Iterate over the folders in the current directory
-for folder in os.listdir(current_directory):
-    folder_path = os.path.join(current_directory, folder)
+for folder in os.listdir(current_directory_portfolio):
+    folder_path = os.path.join(current_directory_portfolio, folder)
     
     # Check if the item is a folder
     if os.path.isdir(folder_path):
@@ -44,23 +53,23 @@ for folder in os.listdir(current_directory):
                 
 
                 # Append the rows to the combined file
-                with open(combined_file_path, 'a') as combined_file:
+                with open(combined_file_path_portfolio, 'a') as combined_file:
                     writer = csv.writer(combined_file)
                     writer.writerows(rows)
                     
 # Remove empty rows from the combined file
-with open(combined_file_path, 'r') as combined_file:
+with open(combined_file_path_portfolio, 'r') as combined_file:
     lines = combined_file.readlines()
 
 # Remove empty rows
 lines = [line for line in lines if line.strip()]
 
 # Write the non-empty lines back to the combined file
-with open(combined_file_path, 'w') as combined_file:
+with open(combined_file_path_portfolio, 'w') as combined_file:
     combined_file.writelines(lines)
 
 # Read the existing content of the combined file
-with open(combined_file_path, 'r') as combined_file:
+with open(combined_file_path_portfolio, 'r') as combined_file:
     existing_content = combined_file.read()
 
 # Append the header row to the existing content
@@ -69,6 +78,60 @@ header_row_str = ','.join(header_row) + '\n'
 new_content = header_row_str + existing_content
 
 # Write the new content back to the combined file
-with open(combined_file_path, 'w') as combined_file:
+with open(combined_file_path_portfolio, 'w') as combined_file:
     combined_file.write(new_content)
     
+
+for folder in os.listdir(current_directory_sector_holding):
+    folder_path = os.path.join(current_directory_sector_holding, folder)
+    
+    # Check if the item is a folder
+    if os.path.isdir(folder_path):
+        # Iterate over the CSV files in the folder
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file)
+            
+            # Check if the item is a CSV file
+            if file.endswith('.csv'):
+                # Read the CSV file
+                with open(file_path, 'r') as csv_file:
+                    reader = csv.reader(csv_file)
+                    rows = list(reader)
+                
+                # Skip the first row
+                rows = rows[1:]
+                
+                # Add a column with the folder name
+                for row in rows:
+                    row.append(folder)
+                    
+                
+
+                # Append the rows to the combined file
+                with open(combined_file_path_sector_holding, 'a') as combined_file:
+                    writer = csv.writer(combined_file)
+                    writer.writerows(rows)
+                    
+# Remove empty rows from the combined file
+with open(combined_file_path_sector_holding, 'r') as combined_file:
+    lines = combined_file.readlines()
+
+# Remove empty rows
+lines = [line for line in lines if line.strip()]
+
+# Write the non-empty lines back to the combined file
+with open(combined_file_path_sector_holding, 'w') as combined_file:
+    combined_file.writelines(lines)
+
+# Read the existing content of the combined file
+with open(combined_file_path_sector_holding, 'r') as combined_file:
+    existing_content = combined_file.read()
+
+# Append the header row to the existing content
+header_row = ['Sector Name', 'Dummy', 'Percentage Allocation', 'Fund Type']
+header_row_str = ','.join(header_row) + '\n'
+new_content = header_row_str + existing_content
+
+# Write the new content back to the combined file
+with open(combined_file_path_sector_holding, 'w') as combined_file:
+    combined_file.write(new_content)
--- mutual_fund_dashboard.py
+++ mutual_fund_dashboard.py
@@ -22,7 +22,7 @@ if "selected_year" not in st.session_state:
 if "selected_month" not in st.session_state:
     st.session_state["selected_month"] = 1
 if "selected_day" not in st.session_state:
-    st.session_state["selected_day"] = 17
+    st.session_state["selected_day"] = 22
 
 selected_year = st.sidebar.selectbox('Select Year', years, index=years.index(st.session_state["selected_year"]))
 selected_month = st.sidebar.selectbox('Select Month', months, index=months.index(st.session_state["selected_month"]))
@@ -44,7 +44,8 @@ current_directory = os.getcwd()
 
 print("Date: "+selected_date)
 # Read the data from the CSV file
-data = pd.read_csv(os.path.join(current_directory, 'daily_mf_data', 'combined_' + selected_date + '.csv'),on_bad_lines='skip')
+data = pd.read_csv(os.path.join(current_directory, 'daily_mf_data','TOP_HOLDING', 'combined_portfolio_' + selected_date + '.csv'),on_bad_lines='skip')
+data_sector = pd.read_csv(os.path.join(current_directory, 'daily_mf_data','TOP_SECTOR_HOLDING', 'combined_sector_' + selected_date + '.csv'),on_bad_lines='skip')
 
 # Define the dropdown options for fund type and asset type
 fund_types = ['All'] + data['Fund Type'].unique().tolist()
@@ -63,8 +64,8 @@ if st.session_state["selected_asset_type"] not in asset_types:
     st.session_state["selected_asset_type"] = "All"
 
 # Create dropdowns for fund type and asset type selection
-st.session_state["selected_fund_type"] = st.selectbox('Select Fund Type', fund_types, index=fund_types.index(st.session_state["selected_fund_type"]))
-st.session_state["selected_asset_type"] = st.selectbox('Select Asset Type', asset_types, index=asset_types.index(st.session_state["selected_asset_type"]))
+st.session_state["selected_fund_type"] = st.sidebar.selectbox('Select Fund Type', fund_types, index=fund_types.index(st.session_state["selected_fund_type"]))
+st.session_state["selected_asset_type"] = st.sidebar.selectbox('Select Asset Type', asset_types, index=asset_types.index(st.session_state["selected_asset_type"]))
 
 # # Rerun the page whenever a selectbox is chosen
 # st.rerun()
@@ -72,26 +73,36 @@ st.session_state["selected_asset_type"] = st.selectbox('Select Asset Type', asse
 # Filter the data based on the selected fund type and asset type
 if st.session_state.selected_fund_type == "All" and st.session_state.selected_asset_type == "All":
     filtered_data = data.copy()
+    filtered_data_sector = data_sector.copy()
 elif st.session_state.selected_fund_type == "All":
     filtered_data = data[data['Asset Type'] == st.session_state.selected_asset_type].copy()
+    filtered_data_sector = data_sector[data_sector['Asset Type'] == st.session_state.selected_asset_type].copy()
 elif st.session_state.selected_asset_type == "All":
     filtered_data = data[data['Fund Type'] == st.session_state.selected_fund_type].copy()
+    filtered_data_sector = data_sector[data_sector['Fund Type'] == st.session_state.selected_fund_type].copy()
 else:
     filtered_data = data[(data['Fund Type'] == st.session_state.selected_fund_type) & (data['Asset Type'] == st.session_state.selected_asset_type)].copy()
+    filtered_data_sector = data_sector[(data_sector['Fund Type'] == st.session_state.selected_fund_type) & (data_sector['Asset Type'] == st.session_state.selected_asset_type)].copy()
 
 # Remove '%' and '-' signs from the Percentage Allocation column and convert it to float type
 filtered_data['Percentage Allocation'] = filtered_data['Percentage Allocation'].str.replace('%', '').str.replace('-', '0').astype(float)
+filtered_data_sector['Percentage Allocation'] = filtered_data_sector['Percentage Allocation'].str.replace('%', '').str.replace('-', '0').astype(float)
 
 # Calculate the sum of the Percentage Allocation column
 sum_percentage_allocation = filtered_data.groupby('Company Name')['Percentage Allocation'].sum()
+sum_percentage_allocation_sector = filtered_data_sector.groupby('Sector Name')['Percentage Allocation'].sum()
 
 # Get the top 10 company names based on sum of Percentage Allocation
 top_10_company_names = sum_percentage_allocation.nlargest(10)
-
+top_10_sector_names = sum_percentage_allocation_sector.nlargest(10)
 
 # Get the top 10 stocks based on frequency of Company Name
 top_10_frequency = filtered_data['Company Name'].value_counts().nlargest(10)
 
+
+# Create a bar chart to illustrate the top 10 stocks by sum of percentage allocation
+fig_sum = px.bar(x=top_10_sector_names.index, y=top_10_sector_names.values, title='Top 10 Sector by Sum of Percentage Allocation')
+st.plotly_chart(fig_sum)
 
 # Create a bar chart to illustrate the top 10 stocks by sum of percentage allocation
 fig_sum = px.bar(x=top_10_company_names.index, y=top_10_company_names.values, title='Top 10 Stocks by Sum of Percentage Allocation')

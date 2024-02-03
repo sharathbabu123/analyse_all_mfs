ef7f7f89f8b2a5e68280eb81935ed5a4b7d91ee7
sharathbabu123 <sharathr013@gmail.com>
2024-02-03 18:13:22 +0530

    Update after run : 03-02-2024--

--- pages/Mutual_Fund_Portfolio_Analysis.py
+++ pages/Mutual_Fund_Portfolio_Analysis.py
@@ -11,71 +11,53 @@ import streamlit as st
 
 st.title('📊 Mutual Fund Portfolio Analysis')
 
-
-
-current_date = str(datetime.date.today())
-# print(current_date)
-
-# Define the dropdown options for year, month, and date
-years = range(2000, datetime.date.today().year + 1)
-months = range(1, 13)
-days = range(1, 32)
-
-# Create the sidebar for date selection
-st.sidebar.title('Date Selection')
-
-# Check if "selected_year", "selected_month", and "selected_day" are in session state, otherwise set them to default values
-if "selected_year" not in st.session_state:
-    st.session_state["selected_year"] = years[-1]
-if "selected_month" not in st.session_state:
-    st.session_state["selected_month"] = 1
-if "selected_day" not in st.session_state:
-    st.session_state["selected_day"] = 22
-
-selected_year = st.sidebar.selectbox('Select Year', years, index=years.index(st.session_state["selected_year"]))
-selected_month = st.sidebar.selectbox('Select Month', months, index=months.index(st.session_state["selected_month"]))
-selected_day = st.sidebar.selectbox('Select Day', days, index=days.index(st.session_state["selected_day"]))
-
-# # Rerun the page whenever a selectbox is chosen
-# st.rerun()
-
-# Update the session state with the selected year, month, and day
-st.session_state["selected_year"] = selected_year
-st.session_state["selected_month"] = selected_month
-st.session_state["selected_day"] = selected_day
-
-# Combine the selected year, month, and date into a single string
-selected_date = str(selected_year) + '-' + str(selected_month).zfill(2) + '-' + str(selected_day).zfill(2)
-
 current_directory = os.getcwd()
+
+# Find the files in the folder "TOP_HOLDING" and sort them
+files = sorted(os.listdir(os.path.join(current_directory, 'daily_mf_data', 'TOP_HOLDING')))
+
+# Select the last file
+last_file = files[-1]
+
+# Save it as a variable as a string
+selected_file = last_file.split('_')[-1].split('.')[0]
+
+
+# current_directory = os.getcwd()
 # current_directory = os.path.dirname(os.path.dirname(os.getcwd()))
 
 # print(current_directory)
 
 # print("Date: "+selected_date)
 # Read the data from the CSV file
-data = pd.read_csv(os.path.join(current_directory, 'daily_mf_data','TOP_HOLDING', 'combined_portfolio_' + selected_date + '.csv'),on_bad_lines='skip')
-data_sector = pd.read_csv(os.path.join(current_directory, 'daily_mf_data','TOP_SECTOR_HOLDING', 'combined_sector_' + selected_date + '.csv'),on_bad_lines='skip')
+data = pd.read_csv(os.path.join(current_directory, 'daily_mf_data','TOP_HOLDING', 'combined_portfolio_' + selected_file+'.csv'),on_bad_lines='skip')
+data_sector = pd.read_csv(os.path.join(current_directory, 'daily_mf_data','TOP_SECTOR_HOLDING', 'combined_sector_' + selected_file+'.csv'),on_bad_lines='skip')
 
 # Define the dropdown options for fund type and asset type
 fund_types = ['All'] + data['Fund Type'].unique().tolist()
 asset_types = ['All'] + data['Asset Type'].unique().tolist()
+scheme_names = ['All'] + data['Scheme Name'].unique().tolist()
 
 # Check if "selected_fund_type" and "selected_asset_type" are in session state, otherwise set them to "All"
 if "selected_fund_type" not in st.session_state:
     st.session_state["selected_fund_type"] = "All"
 if "selected_asset_type" not in st.session_state:
     st.session_state["selected_asset_type"] = "All"
+if "selected_scheme_name" not in st.session_state:
+    st.session_state["selected_scheme_name"] = "All"
 
 # Check if the selected fund type and asset type are valid, otherwise set them to "All"
 if st.session_state["selected_fund_type"] not in fund_types:
     st.session_state["selected_fund_type"] = "All"
 if st.session_state["selected_asset_type"] not in asset_types:
     st.session_state["selected_asset_type"] = "All"
+if st.session_state["selected_scheme_name"] not in scheme_names:
+    st.session_state["selected_scheme_name"] = "All"
 
 # Create dropdowns for fund type and asset type selection
 st.session_state["selected_fund_type"] = st.sidebar.selectbox('Select Fund Type', fund_types, index=fund_types.index(st.session_state["selected_fund_type"]))
 st.session_state["selected_asset_type"] = st.sidebar.selectbox('Select Asset Type', asset_types, index=asset_types.index(st.session_state["selected_asset_type"]))
+st.session_state["selected_scheme_name"] = st.sidebar.selectbox('Select Scheme Name', scheme_names, index=scheme_names.index(st.session_state["selected_scheme_name"]))
 
 # # Rerun the page whenever a selectbox is chosen
 # st.rerun()
@@ -93,6 +75,10 @@ elif st.session_state.selected_asset_type == "All":
 else:
     filtered_data = data[(data['Fund Type'] == st.session_state.selected_fund_type) & (data['Asset Type'] == st.session_state.selected_asset_type)].copy()
     filtered_data_sector = data_sector[(data_sector['Fund Type'] == st.session_state.selected_fund_type)].copy()
+
+if st.session_state.selected_scheme_name != "All":
+    filtered_data = filtered_data[filtered_data['Scheme Name'] == st.session_state.selected_scheme_name]
+    filtered_data_sector = filtered_data_sector[filtered_data_sector['Scheme Name'] == st.session_state.selected_scheme_name]
 
 # Remove '%' and '-' signs from the Percentage Allocation column and convert it to float type
 filtered_data['Percentage Allocation'] = filtered_data['Percentage Allocation'].str.replace('%', '').str.replace('-', '0').astype(float)
